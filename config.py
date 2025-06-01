938582e6ebcae185a3f51280406d03e4ba144747
Harmanpreet Singh <146160902+ohiharman@users.noreply.github.com>
2025-06-01 08:09:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config(object):
       API_HASH = getenv("API_HASH", "90b8bad42e6210d0e1e04a858e045f55")
       API_ID = int(getenv("API_ID", "27680167"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
-      BOT_TOKEN = getenv("BOT_TOKEN", "mongodb+srv://abohariyaharman2:lRekbm3GDSk0rL3v@cluster0.xfu5dov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1002670957174:-1002588279501").replace("\n", " ").split(' '))
+      BOT_TOKEN = getenv("BOT_TOKEN", "")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(' '))
 
 
 # Don't Remove Credit @VJ_Botz
